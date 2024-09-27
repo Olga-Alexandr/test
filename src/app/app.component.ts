0af3bfb7b6f7764b3d23ea629c003ee79a16f158
Olga S. <jkmuf3@gmail.com>
2024-09-27 23:49:39 +0300

    that's a start.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgFor, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss', './style.scss'],
 })
 export class AppComponent {
   title = 'test';
+  public test: string = 'abcde';
+  // public name: string = 'john';
+  // public surn: string = 'smith';
+  public src = '{{src}}';
+  public alt = '{{alt}}}';
+  public arr: number[] = [1, 2, 3, 4, 5];
+  public user = {
+		name: 'john',
+		surn: 'smit',
+    img: 'pic.png',
+		alt: 'picture',
+	};
+
+  public showName() {
+		return 'john';
+	}
+
+	public showSurn() {
+		return 'smit';
+	}
+
+  public isAdmin: boolean = true;
 }
+
